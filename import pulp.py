8f7779a6b946a4682e311eaafeaecbf51a23e76d
Polloensarten <cuevassantiago97@gmail.com>
2026-05-27 20:33:36 -0600

    trabajo

--- import pulp.py
+++ import pulp.py
@@ -1,18 +1,18 @@
 import pulp
 
 # 1. Definir el problema (Minimización)
-model = pulp.LpProblem("Cloud_Optimization", pulp.LpMinimize)
+model = pulp.LpProblem("Cloud_Optimization", pulp.LpMaximize)
 
 # 2. Definir Variables (Enteras, ya que no puedes rentar media instancia)
-x1 = pulp.LpVariable("Estandar",cat='Integer')
-x2 = pulp.LpVariable("Premium",cat='Integer')
+x1 = pulp.LpVariable("Personaje",cat='Integer')
+x2 = pulp.LpVariable("Escenario",cat='Integer')
 
 # 3. Función Objetivo
-model += 20 * x1 + 60 * x2, "Costo_Total"
+model += 80 * x1 + 60 * x2, "Costo_Total"
 
 # 4. Restricciones
-model += 1 * x1 + 3 * x2 >= 15, "Unidades de velocidad"
-model += 2 * x1 + 2 * x2 >= 14, "Dias de retencion"
+model += 2 * x1 + 1 * x2 <= 12, "Tiempo GPU"
+model += 1 * x1 + 2 * x2 <= 14, "Memoria de video"
 
 # 5. Resolver y mostrar
 model.solve()
